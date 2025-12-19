a6e753b563bb198eb92559cc958d6a99aa268bc4
joudabdelkhaleq <joudabd47@gmail.com>
2025-12-19 09:55:06 +0200

    AddFragment

--- app/src/main/java/com/example/myapplication14/AddCarFragment.java
+++ app/src/main/java/com/example/myapplication14/AddCarFragment.java
@@ -2,15 +2,25 @@ package com.example.myapplication14;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
 
 
 public class AddCarFragment extends Fragment {
-    private
+    private EditText etName , etLocation ,etPhone ,etNumPlate , etCategory;
+    private Button btnAdd ;
+    private FirebaseServices fbs ;
 
 
     @Override
@@ -26,7 +36,40 @@ public class AddCarFragment extends Fragment {
     public void connectComponents()
     {
         etName =getActivity().findViewById(R.id.etNameAddCarFragment);
-        et
+        etLocation = getActivity().findViewById(R.id.etLocationAddCarFragment);
+        etPhone= getActivity().findViewById(R.id.etPhoneAddCarFragment);
+        etNumPlate=getActivity().findViewById(R.id.etNumPlateAddCarFragment);
+        etCategory = getActivity().findViewById(R.id.etCategoryAddCarFragment);
+        fbs = FirebaseServices.getInstance();
+        btnAdd = getActivity().findViewById(R.id.btnAddAddCarFragment);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name,location,phone,numberplate,category;
+                name = etName.getText().toString();
+                location = etLocation.getText().toString();
+                phone = etPhone.getText().toString();
+                numberplate = etNumPlate.getText().toString();
+                category = etCategory.getText().toString();
+                if (name.trim().isEmpty() || location.trim().isEmpty() || phone.trim().isEmpty() || numberplate.trim().isEmpty() || category.trim().isEmpty()){
+                    Toast.makeText(getActivity(), "some fields are empty!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Car car = new Car(name,location,phone,,category,"");
+
+                fbs.getFire().collection("cars").add(car).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/myapplication14/Car.java
+++ app/src/main/java/com/example/myapplication14/Car.java
@@ -4,18 +4,20 @@ public class Car {
     private String name ;
     private String phone ;
     private String photo ;
-    private int prise ;
+    private int price;
     private String category ;
     private String location ;
+    private String numberplate;
 
-    public Car(String name , String phone , String photo , int prise,String category,String location)
+    public Car(String name , String phone , String photo , int price,String category,String location,String numberplate)
     {
      this.name = name;
      this.phone = phone;
      this.photo = photo;
-     this.prise = prise;
+     this.price = price;
      this.category=category;
      this.location=location;
+     this.numberplate=numberplate;
     }
     public String getName(){
         return name ;
@@ -26,11 +28,12 @@ public class Car {
     public String getPhoto(){
         return photo ;
     }
-    public int getPrise()
+    public int getPrice()
     {
-        return prise ;
+        return price;
     }
     public String getCategory(){return category;}
     public String getLocation(){return location;}
+    public String getNumberplate(){return numberplate;}
 
 }
