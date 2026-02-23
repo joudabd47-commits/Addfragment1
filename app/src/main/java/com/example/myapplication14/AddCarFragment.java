9246b7213c7f3c3cf91c46be360760c06c03c9d9
joudabdelkhaleq <joudabd47@gmail.com>
2026-02-23 12:50:32 +0200

    AddFragment

--- app/src/main/java/com/example/myapplication14/AddCarFragment.java
+++ app/src/main/java/com/example/myapplication14/AddCarFragment.java
@@ -59,7 +59,6 @@ public class AddCarFragment extends Fragment {
                 }
                 Car car = new Car(name,phone, price,category,location,numberplate);
                 // (String name , String phone , String photo , int price,String category,String location,String numberplate)
-                fbs.getAuth().createUserWithEmailAndPassword(user,pass).
 
                 fbs.getFire().collection("cars").add(car).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                     @Override
